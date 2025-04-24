036825cbc427a71a53937ae7216759c21126f704
Arjun Malhotra <arjunhero38@gmail.com>
2025-04-24 19:10:21 +0530

    nice

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./Context/AuthContext.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
+  <Analytics>
   <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
   </StrictMode>
+  </Analytics>
 );
